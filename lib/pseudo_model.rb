8b1fd45edd2938cfb32d962fb034dcba5d46ac93
Ryan Scott Lewis <ryan@rynet.us>
2013-03-07 09:52:38 -0500

    Add validation and conversion to the `attributes` argument

--- lib/pseudo_model.rb
+++ lib/pseudo_model.rb
@@ -6,6 +6,9 @@ module PseudoModel
     include ActiveModel::Conversion
 
     def initialize(attributes = {})
+      raise TypeError, '`attributes` must be a Hash or respond to :to_hash or :to_h' unless attributes.is_a?(Hash) || attributes.respond_to?(:to_hash) || attributes.respond_to?(:to_h)
+      attributes = attributes.to_hash rescue attributes.to_h unless attributes.is_a?(Hash)
+      
       attributes.each do |name, value|
         send("#{name}=", value)
       end
